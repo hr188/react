5f17d6fc0f71da81d93778d5b17fd98789591277
hr188 <himanshuray188@gmail.com>
2023-06-24 11:26:41 +0530

    starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+const buttonn=  ()=> {
+    return(
+    <>
+        <h1> hello</h1>
+        <h2>  hiiiee</h2>
+    </>
+    )
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const name = "himanshu";
+    const isnameShowing = true;
+    return (<>
+    <div className='App'>
+        <h1>hello {isnameShowing ? name:'nahi hai'}!</h1>
     </div>
-  );
+    </>
+    );
 }
 
 export default App;
